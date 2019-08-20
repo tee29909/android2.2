23d47622bdcf272b00557eca36f144092b76bb4b
Theerapat Nugud <59160571@go.buu.ac.th>
2019-08-21 01:14:22 +0700

    Add a click listener to the nickname TextView

--- app/src/main/java/buu/informatics/s59160571/aboutme/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160571/aboutme/MainActivity.kt
@@ -17,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
         }
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickname(it)
+        }
     }
     private fun addNickname(view: View) {
         val editText = findViewById<EditText>(R.id.nickname_edit)
@@ -30,6 +33,22 @@ class MainActivity : AppCompatActivity() {
         doneButton.visibility = View.GONE
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
+
+    }
+    private fun updateNickname (view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        val doneButton = findViewById<Button>(R.id.done_button)
+        editText.visibility = View.VISIBLE
+        view.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        nicknameTextView.visibility = View.GONE
+        editText.requestFocus()
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
+
+
 
 
     }
